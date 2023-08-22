d3c9810f103972299f9e9213f3f4e2d9fd9ee068
ArthurDev <arthurdevoficial@gmail.com>
2023-08-22 16:33:54 -0300

    feat: Implemented mobile menu

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,9 +1,18 @@
+import MobileMenu from '../MobileMenu';
 import './index.css';
 
 export default function Topbar() {
     return (
-        <nav className="topbar">
+        <div className="topbar">
             <h1 className="tobpar-title">YogaMaster</h1>
-        </nav>
+
+			<nav className="menu">
+				<a className="menu-item strong-font" href="#about-us">About us</a>
+				<a className="menu-item strong-font" href="#classes">See our classes</a>
+				<a className="menu-item strong-font" href="#contact">Get in touch</a>
+			</nav>
+
+			<MobileMenu />
+        </div>
     )
 }
